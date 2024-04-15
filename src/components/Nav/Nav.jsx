931c2a61d62872a878b5469441671de08f8d2c8b
Richard T <74796181+rtillies@users.noreply.github.com>
2024-04-15 15:45:29 -0400

    Add BrowserRouter

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,24 +1,26 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import './nav.css'
+import React from "react";
+import { BrowserRouter, Link } from "react-router-dom";
+import "./nav.css";
 
 export default () => {
   return (
     <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/all">All Cards</Link>
-        </li>
-        <li>
-          <Link to="/suit">By Suit</Link>
-        </li>
-        <li>
-          <Link to="/rank">By Rank</Link>
-        </li>
-      </ul>
+      <BrowserRouter>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/all">All Cards</Link>
+          </li>
+          <li>
+            <Link to="/suit">By Suit</Link>
+          </li>
+          <li>
+            <Link to="/rank">By Rank</Link>
+          </li>
+        </ul>
+      </BrowserRouter>
     </nav>
   );
 };
