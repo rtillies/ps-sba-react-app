8991b15a10edb5f3f9f98896bfc13e59c91f544b
Richard T <74796181+rtillies@users.noreply.github.com>
2024-04-15 17:18:57 -0400

    Add use Context

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import './landing.css'
+import { DeckContext } from "../../Context";
 
 export default () => {
+  const deckID = useContext(DeckContext);
+
   return (
     <>
     <div className='landing page'>
       <h1>Welcome to the Thunderdome</h1>
+      <p>Card Deck: {deckID}</p>
     </div>
     </>
   )
