fe9913f4b4d616169d76532596389eaf386d8c2e
Richard T <74796181+rtillies@users.noreply.github.com>
2024-04-15 19:18:32 -0400

    Cleanup

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,16 +1,16 @@
-import React, {useContext} from 'react'
-import './landing.css'
-import { DeckContext } from "../../Context";
+import React, { useContext } from "react";
+import "./landing.css";
+import { DeckContext } from "../../components/Context";
 
 export default () => {
   const deckID = useContext(DeckContext);
 
   return (
     <>
-    <div className='landing page'>
-      <h1>Welcome to the Thunderdome</h1>
-      <p>Card Deck: {deckID}</p>
-    </div>
+      <div className="landing page">
+        <h1>Welcome to the Thunderdome</h1>
+        <p>Card Deck: {deckID}</p>
+      </div>
     </>
-  )
-}
+  );
+};
