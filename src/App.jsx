09e9e546392401344c56636b48bc7e2916e5c47e
Richard T <74796181+rtillies@users.noreply.github.com>
2024-04-15 13:35:08 -0400

    use Effect to get deck, then use context to send deck to all routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,49 @@
-import { useState } from 'react'
-import {Route, Routes} from 'react-router-dom';
+import { useState, useEffect, useContext } from "react";
+import { Route, Routes } from "react-router-dom";
+import axios from "axios";
 
-import './App.css'
-import Nav from './components/Nav/Nav'
-import Custom404 from './pages/Custom404/Custom404'
-
+// import "./App.css";
+import Nav from "./components/Nav/Nav";
+import Landing from "./pages/Landing/Landing";
+import AllCards from "./pages/AllCards/AllCards";
+import Custom404 from "./pages/Custom404/Custom404";
+import { DeckContext } from "./Context";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [deck, setDeck] = useState("06tjvcttxnld");
+
+  //#region Use Effect: Fetch Card Deck
+  useEffect(() => {
+    async function getDeck() {
+      if (!deck) setDeck("new");
+      axios
+        .get(
+          `${deck}/shuffle/?jokers_enabled=true`
+        )
+        .then((res) => {
+          console.log(res.data);
+          setDeck(res.data.deck_id);
+        });
+    }
+    getDeck();
+  }, []);
+  //#endregion
 
   return (
     <>
-      <div className='App'>
+      <div className="App">
         <Nav />
 
-        <Routes>
-          {/* <Route path='/' element={<Landing />} />
-          <Route path='/home' element={<Home />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/display' element={<Single />} />
-          <Route path='*' element={<Custom404 />} /> */}
-          <Route path='*' element={<Custom404 />} />
-        </Routes>
-
+        <DeckContext.Provider value={deck}>
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/all" element={<AllCards />} />
+            <Route path="*" element={<Custom404 />} />
+          </Routes>
+        </DeckContext.Provider>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
