1b089b528f6f3547a8b221d86c58631639082297
Richard T <74796181+rtillies@users.noreply.github.com>
2024-04-15 17:19:43 -0400

    getCards function works

--- src/stores/cardsStore.jsx
+++ src/stores/cardsStore.jsx
@@ -4,26 +4,36 @@ import axios from "axios";
 const cardsStore = create((set) => ({
   deck: null,
   deckID: '06tjvcttxnld',
+  // deckID: null,
 
   // getDeck: async() => {
-  //   const deckID = getState() || 'new'
+  //   // const {deckID} = cardsStore.getState() || 'new'
   //   // const id = deckID || 'new'
   //   console.log(deckID);
 
   //   const res = await axios.get(`${deckID}/shuffle/?jokers_enabled=true`)
   //   set({
-  //     deckID: res.data.deckID
+  //     // deckID: res.data.deckID
   //   })
   // },
 
   getCards: async() => {
-    const deckID = cardsStore.getState() || 'new'
-    // console.log(deckID);
-    const res = await axios.get(`${deckID}/draw/?count=${3}`)
-    // console.log(res.data.cards);
+    const {deckID} = cardsStore.getState() || 'new'
+    const count = 2
+
+    if (deckID === null) {
+      // set({deckID: 'new'})
+    }
+
+    // const {deckID} = cardsStore.getState() || 'new'
+
+    await axios.get(`${deckID}/shuffle/`)
+    const res = await axios.get(`${deckID}/draw/?count=${count}`)
+
+    console.log(deckID);
+    console.log('data', res.data.cards);
     set({
-      // deck: res.data.cards
-      deck: res.data
+      deck: res.data.cards
     })
   },
 
