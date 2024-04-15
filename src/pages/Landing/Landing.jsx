308ae413ba542496b894f06d334f9d4d78f35fed
Richard T <74796181+rtillies@users.noreply.github.com>
2024-04-15 12:40:12 -0400

    Landing page from previous project

---
+++ src/pages/Landing/Landing.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './landing.css'
+
+export default () => {
+  return (
+    <>
+    <div className='landing page'>
+      <h1>Welcome to the Thunderdome</h1>
+    </div>
+    </>
+  )
+}
