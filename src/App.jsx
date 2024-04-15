28750cb6a9b10275372a4d58b6ce12439f1504a3
Richard T <74796181+rtillies@users.noreply.github.com>
2024-04-15 11:52:54 -0400

    start creating app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import { useState } from 'react'
+import {Route, Routes} from 'react-router-dom';
+
 import './App.css'
+import Nav from './components/Nav/Nav'
+import Custom404 from './pages/Custom404/Custom404'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,6 +12,17 @@ function App() {
   return (
     <>
       <div className='App'>
+        <Nav />
+
+        <Routes>
+          {/* <Route path='/' element={<Landing />} />
+          <Route path='/home' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/display' element={<Single />} />
+          <Route path='*' element={<Custom404 />} /> */}
+          <Route path='*' element={<Custom404 />} />
+        </Routes>
+
       </div>
     </>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import { StrictMode } from "react";
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App'
 import './index.css'
 
+// Import BrowserRouter
+import { BrowserRouter } from 'react-router-dom';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>,
 )
+
