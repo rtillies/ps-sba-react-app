ccfe5857fe8278d6d453ac86ea952fd9787b32f4
Richard T <74796181+rtillies@users.noreply.github.com>
2024-04-15 17:23:45 -0400

    Display card image

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,6 +4,9 @@ export default function Card({card}) {
   const store = cardsStore();
 
   return (
-    <div key={card.code}>Card</div>
+    <div key={card.code}>
+      {/* Card {card.code} */}
+      <img src={card.image} alt={card.code} height={100} />
+    </div>
   )
 }
