04fc7b81eb330292cc3a096b701a877cfacfe73b
Richard T <74796181+rtillies@users.noreply.github.com>
2024-04-15 21:59:33 -0400

    Remove labels

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -6,10 +6,11 @@ export default function Card({card, position}) {
 
   return (
     <div className="card-group" key={card.code}>
-      <p className="top">{position}</p>
+      {/* <p className="top">{position}</p> */}
+      {/* <br /> */}
       <img className="Card" src={card.image} alt={card.code} />
-      <br />
-      <p className="bottom">{card.value} {card.suit}</p>
+      {/* <br /> */}
+      {/* <p className="bottom">{card.value} {card.suit}</p> */}
     </div>
   )
 }
