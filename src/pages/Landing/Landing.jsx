fe38c07e0e9e360c3ab7fd5646137aa0bf1a6b7a
Richard T <74796181+rtillies@users.noreply.github.com>
2024-04-15 20:32:59 -0400

    deck id

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,15 +1,17 @@
 import React, { useContext } from "react";
 import "./landing.css";
 import { DeckContext } from "../../components/Context";
+import cardsStore from "../../stores/cardsStore";
 
 export default () => {
-  const deckID = useContext(DeckContext);
+  // const deckID = useContext(DeckContext);
+  const store = cardsStore();
 
   return (
     <>
       <div className="landing page">
         <h1>Welcome to the Thunderdome</h1>
-        <p>Card Deck: {deckID}</p>
+        <span className="small">Card Deck ID: {store.deckID}</span>
       </div>
     </>
   );
