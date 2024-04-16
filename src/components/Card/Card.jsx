032068a2eefde00ba3355dba72548bf9d29559b3
Richard T <74796181+rtillies@users.noreply.github.com>
2024-04-15 20:46:53 -0400

    Card function and styling

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,15 +1,15 @@
 import cardsStore from "../../stores/cardsStore"
 import './card.css'
 
-export default function Card({card}) {
+export default function Card({card, position}) {
   const store = cardsStore();
 
   return (
     <div className="card-group" key={card.code}>
-      {/* Card {card.code} */}
+      <p className="top">{position}</p>
       <img className="Card" src={card.image} alt={card.code} />
       <br />
-      <p className="words">{card.value} {card.suit}</p>
+      <p className="bottom">{card.value} {card.suit}</p>
     </div>
   )
 }
