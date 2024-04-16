30a2ea1ba8352c24e9679314214f12ab1e0e00c7
Richard T <74796181+rtillies@users.noreply.github.com>
2024-04-15 21:34:22 -0400

    Apply bootstrap

--- src/pages/Filter/Filter.jsx
+++ src/pages/Filter/Filter.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState, useRef, useContext } from "react";
 import axios from "axios";
+import 'bootstrap/dist/css/bootstrap.css';
 import { DeckContext } from "../../components/Context";
 import cardsStore from "../../stores/cardsStore";
 import Deck from "../../components/Deck/Deck";
@@ -22,17 +23,16 @@ export default () => {
     <>
       <div className="ranks page">
         <div className="container">
-        {/* <h1>Filter</h1> */}
         <h3>Click a button to filter cards</h3>
           {store.suits && store.suits.map((suit) => {
-            return <button key={suit} value={suit} onClick={handleClick}>{suit}</button>
+            return <button className="btn btn-outline-dark btn-sm" key={suit} value={suit} onClick={handleClick}>{suit}</button>
           })}
         <br />
           {store.ranks && store.ranks.map((rank) => {
-            return <button key={rank} value={rank} onClick={handleClick}>{rank}</button>
+            return <button className="btn btn-outline-dark btn-sm" key={rank} value={rank} onClick={handleClick}>{rank}</button>
           })}
           <br />
-          <button id='shuffle' key='shuffle' value='shuffle' onClick={store.getCards}>SHUFFLE CARDS</button>
+          <button className="btn btn-primary btn-sm" id='shuffle' key='shuffle' value='shuffle' onClick={store.getCards}>SHUFFLE CARDS</button>
           <Deck />
           <span className="small">Card Deck ID: {store.deckID}</span>
         </div>
